cc08e315f6d94d1b0c2f6519415c4d899fdda09f
Tomi Takussaari <tomi.takussaari@gmail.com>
2015-06-14 19:18:48 +0300

    Verify proxied connection is usable

--- mysqlcluscon-driver/src/test/java/com/github/tomitakussaari/mysqlcluscon/MysclusconDriverTest.java
+++ mysqlcluscon-driver/src/test/java/com/github/tomitakussaari/mysqlcluscon/MysclusconDriverTest.java
@@ -3,18 +3,15 @@ package com.github.tomitakussaari.mysqlcluscon;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -22,35 +19,45 @@ import static org.mockito.Mockito.when;
 public class MysclusconDriverTest {
 
     @Mock
-    Connection conn;
+    Connection mockConn;
     @Mock
-    Statement statement;
+    Statement mockStatement;
     @Mock
-    ResultSet resultSet;
+    ResultSet mockResultSet;
 
     TestableMysqlclusconDriver driver = new TestableMysqlclusconDriver();
 
     @Test
     public void galeraCluster() throws SQLException {
-        when(conn.createStatement()).thenReturn(statement);
-        when(conn.isValid(anyInt())).thenReturn(true);
-        when(statement.executeQuery("SHOW STATUS like 'wsrep_ready'")).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true);
-        when(resultSet.getString("Value")).thenReturn("ON");
+        when(mockConn.createStatement()).thenReturn(mockStatement);
+        when(mockConn.isValid(anyInt())).thenReturn(true);
+        when(mockStatement.executeQuery("SHOW STATUS like 'wsrep_ready'")).thenReturn(mockResultSet);
+        when(mockResultSet.next()).thenReturn(true);
+        when(mockResultSet.getString("Value")).thenReturn("ON");
 
         Connection connection = driver.connect("jdbc:myscluscon:galera:cluster://A,B,C", new Properties());
         assertNotNull(connection);
         assertTrue(connection.isValid(1));
+
+        verifyConnectionIsUsable(connection);
+    }
+
+    private void verifyConnectionIsUsable(Connection connection) throws SQLException {
+        PreparedStatement mockPs = Mockito.mock(PreparedStatement.class);
+        ResultSet mockRs = Mockito.mock(ResultSet.class);
+        when(mockConn.prepareStatement("SELECT * FROM FOOBAR")).thenReturn(mockPs);
+        when(mockPs.executeQuery()).thenReturn(mockRs);
+        assertSame(mockRs, connection.prepareStatement("SELECT * FROM FOOBAR").executeQuery());
     }
 
     @Test
     public void triesNodesUntilFindsWorkingOneWhenGalera() throws SQLException {
-        when(conn.createStatement()).thenReturn(statement);
-        when(conn.isValid(anyInt())).thenReturn(false).thenReturn(false).thenReturn(true);
+        when(mockConn.createStatement()).thenReturn(mockStatement);
+        when(mockConn.isValid(anyInt())).thenReturn(false).thenReturn(false).thenReturn(true);
 
-        when(statement.executeQuery("SHOW STATUS like 'wsrep_ready'")).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(false).thenReturn(false).thenReturn(true);
-        when(resultSet.getString("Value")).thenReturn("ON");
+        when(mockStatement.executeQuery("SHOW STATUS like 'wsrep_ready'")).thenReturn(mockResultSet);
+        when(mockResultSet.next()).thenReturn(false).thenReturn(false).thenReturn(true);
+        when(mockResultSet.getString("Value")).thenReturn("ON");
 
         driver.connect("jdbc:myscluscon:galera:cluster://A,B,C:1234?foo=bar&bar=foo", new Properties());
         assertTrue(driver.connectUrls.contains("jdbc:mysql://A:1234?foo=bar&bar=foo"));
@@ -60,28 +67,30 @@ public class MysclusconDriverTest {
 
     @Test
     public void mysqlCluster() throws SQLException {
-        when(conn.createStatement()).thenReturn(statement);
-        when(conn.isValid(anyInt())).thenReturn(true);
-        when(statement.executeQuery("SHOW SLAVE STATUS")).thenReturn(resultSet);
-        when(resultSet.getObject("Slave_IO_Running")).thenReturn("Yes");
-        when(resultSet.getObject("Slave_SQL_Running")).thenReturn("Yes");
-        when(resultSet.getObject("Seconds_Behind_Master")).thenReturn("0");
-        when(resultSet.next()).thenReturn(true);
+        when(mockConn.createStatement()).thenReturn(mockStatement);
+        when(mockConn.isValid(anyInt())).thenReturn(true);
+        when(mockStatement.executeQuery("SHOW SLAVE STATUS")).thenReturn(mockResultSet);
+        when(mockResultSet.getObject("Slave_IO_Running")).thenReturn("Yes");
+        when(mockResultSet.getObject("Slave_SQL_Running")).thenReturn("Yes");
+        when(mockResultSet.getObject("Seconds_Behind_Master")).thenReturn("0");
+        when(mockResultSet.next()).thenReturn(true);
 
         Connection connection = driver.connect("jdbc:myscluscon:mysql:read_cluster://A,B,C", new Properties());
         assertNotNull(connection);
         assertTrue(connection.isValid(1));
+
+        verifyConnectionIsUsable(connection);
     }
 
     @Test
     public void triesNodesUntilFindsWorkingOneWhenMysqlCluster() throws SQLException {
-        when(conn.createStatement()).thenReturn(statement);
-        when(conn.isValid(anyInt())).thenReturn(false).thenReturn(false).thenReturn(true);
-        when(statement.executeQuery("SHOW SLAVE STATUS")).thenReturn(resultSet);
-        when(resultSet.getObject("Slave_IO_Running")).thenReturn("Yes");
-        when(resultSet.getObject("Slave_SQL_Running")).thenReturn("Yes");
-        when(resultSet.getObject("Seconds_Behind_Master")).thenReturn("0");
-        when(resultSet.next()).thenReturn(true);
+        when(mockConn.createStatement()).thenReturn(mockStatement);
+        when(mockConn.isValid(anyInt())).thenReturn(false).thenReturn(false).thenReturn(true);
+        when(mockStatement.executeQuery("SHOW SLAVE STATUS")).thenReturn(mockResultSet);
+        when(mockResultSet.getObject("Slave_IO_Running")).thenReturn("Yes");
+        when(mockResultSet.getObject("Slave_SQL_Running")).thenReturn("Yes");
+        when(mockResultSet.getObject("Seconds_Behind_Master")).thenReturn("0");
+        when(mockResultSet.next()).thenReturn(true);
 
         driver.connect("jdbc:myscluscon:mysql:read_cluster://A,B,C:1234?foo=bar&bar=foo", new Properties());
         assertTrue(driver.connectUrls.contains("jdbc:mysql://A:1234?foo=bar&bar=foo"));
@@ -94,7 +103,7 @@ public class MysclusconDriverTest {
         @Override
         protected Connection openRealConnection(Properties info, String connectUrl) throws SQLException {
             connectUrls.add(connectUrl);
-            return conn;
+            return mockConn;
         }
     }
 
