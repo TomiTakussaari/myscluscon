4465d6606c16447b90bc4d7341e618e63b1502c3
Tomi Takussaari <tomi.takussaari@gmail.com>
2016-10-30 18:32:37 +0200

    Mark Params as utility class

--- src/main/java/com/github/tomitakussaari/mysqlcluscon/Params.java
+++ src/main/java/com/github/tomitakussaari/mysqlcluscon/Params.java
@@ -2,6 +2,10 @@ package com.github.tomitakussaari.mysqlcluscon;
 
 class Params {
 
+    private Params() {
+        //just a place to store parameters
+    }
+
     static final Integer DEFAULT_CONNECT_TIMEOUT_IN_MS = 500;
     static final String MYSQL_CONNECT_TIMEOUT_PARAM = "connectTimeout";
 
