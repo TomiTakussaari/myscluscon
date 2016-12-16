b342ba5fd520c7eec8ac400d16db139ca351a390
Tomi Takussaari <tomi.takussaari@gmail.com>
2016-12-16 16:26:49 +0200

    Remove unused private constructor
    Class is package private, so chances for someone to instantiate it are slim...

--- src/main/java/com/github/tomitakussaari/mysqlcluscon/Params.java
+++ src/main/java/com/github/tomitakussaari/mysqlcluscon/Params.java
@@ -1,12 +1,6 @@
 package com.github.tomitakussaari.mysqlcluscon;
 
 class Params {
-
-    private Params() {
-        //just a place to store parameters
-    }
-
     static final Integer DEFAULT_CONNECT_TIMEOUT_IN_MS = 500;
     static final String MYSQL_CONNECT_TIMEOUT_PARAM = "connectTimeout";
-
 }
